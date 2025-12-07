f0fe828e9bc9f5bde7986f9c432c6e63101eb24d
davenanother-pixel <davenanother@gmail.com>
2025-12-07 11:44:05 -1000

    Implement company creation and rendering functions
    Added functions to create and render companies.

--- companies.js
+++ companies.js
@@ -9,7 +9,25 @@ function investInCompany(index) {
         money -= companies[index].value;
         alert(`Invested in: ${companies[index].name}`);
         updateDisplay();
+        renderCompanies();
     } else {
         alert("Not enough money!");
     }
 }
+
+function createCustomCompany(name, value) {
+    if (!name || !value) return alert("Name and value required!");
+    companies.push({ name, value });
+    alert(`Created company: ${name} valued at $${value}`);
+    renderCompanies();
+}
+
+function renderCompanies() {
+    const container = document.getElementById("companiesList");
+    container.innerHTML = "";
+    companies.forEach((company, i) => {
+        container.innerHTML += `<div>${company.name} - $${company.value}</div>`;
+    });
+}
+setInterval(renderCompanies, 1000);
+
