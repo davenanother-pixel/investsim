ad808fae2aecd3b89242a981595b880d88508d6b
davenanother-pixel <davenanother@gmail.com>
2025-12-07 11:41:10 -1000

    Add car objects and buyCar function

--- cars.js
+++ cars.js
@@ -1,1 +1,16 @@
+let cars = [
+    { name: "Compact Car", price: 15000 },
+    { name: "Sedan", price: 30000 },
+    { name: "Sports Car", price: 100000 }
+];
+
+function buyCar(index) {
+    if (money >= cars[index].price) {
+        money -= cars[index].price;
+        alert(`You bought: ${cars[index].name}`);
+        updateDisplay();
+    } else {
+        alert("Not enough money!");
+    }
+}
 
