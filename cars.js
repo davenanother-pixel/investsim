fefaedb0c7e2556a41a2e57d8aed18307f8ec816
davenanother-pixel <davenanother@gmail.com>
2025-12-07 11:43:43 -1000

    Add custom car creation and rendering functionality

--- cars.js
+++ cars.js
@@ -9,8 +9,24 @@ function buyCar(index) {
         money -= cars[index].price;
         alert(`You bought: ${cars[index].name}`);
         updateDisplay();
+        renderCars();
     } else {
         alert("Not enough money!");
     }
 }
 
+function createCustomCar(name, price) {
+    if (!name || !price) return alert("Name and price required!");
+    cars.push({ name, price });
+    alert(`Created car: ${name} for $${price}`);
+    renderCars();
+}
+
+function renderCars() {
+    const container = document.getElementById("carsList");
+    container.innerHTML = "";
+    cars.forEach((car, i) => {
+        container.innerHTML += `<div>${car.name} - $${car.price}</div>`;
+    });
+}
+setInterval(renderCars, 1000);
