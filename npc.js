2e2b3294910877d885c040647c20094fac6ba64d
davenanother-pixel <davenanother@gmail.com>
2025-12-07 14:29:54 -1000

    Update npc.js

--- npc.js
+++ npc.js
@@ -1,13 +1,1 @@
-setInterval(() => {
-    stocks.forEach(stock => {
-        let change = (Math.random() - 0.5) * 5;
-        stock.price = Math.max(1, Math.floor(stock.price + change));
-
-        let action = Math.random();
-        if (action < 0.05 && stock.npcShares > 0) {
-            stock.npcShares -= 1;
-        } else if (action < 0.1) {
-            stock.npcShares += 1;
-        }
-    });
-}, 1500);
+console.log("NPC system loaded");
