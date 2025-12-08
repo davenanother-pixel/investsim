9cf579cfe57ee6ac6c30f39c33ba3186a913c7c5
davenanother-pixel <davenanother@gmail.com>
2025-12-07 14:29:16 -1000

    Update homes.js

--- homes.js
+++ homes.js
@@ -1,21 +1,2 @@
 let homes = [];
-
-function createHome() {
-    if (money >= 50000) {
-        money -= 50000;
-        homes.push({ name: `Home #${homes.length + 1}`, value: 50000 });
-        renderHomes();
-    } else {
-        alert("Not enough money for a home!");
-    }
-}
-
-function renderHomes() {
-    const div = document.getElementById("yourHomes");
-    div.innerHTML = "";
-    homes.forEach(home => {
-        div.innerHTML += `${home.name} | $${home.value}<br>`;
-    });
-}
-
-setInterval(renderHomes, 500);
+console.log("Homes system loaded (placeholder)");
