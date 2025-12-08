37796d911801f6a7979b7439741270f43b197be8
davenanother-pixel <davenanother@gmail.com>
2025-12-07 14:29:33 -1000

    Update cars.js

--- cars.js
+++ cars.js
@@ -1,22 +1,2 @@
 let cars = [];
-
-function createCar() {
-    if (money >= 10000) {
-        money -= 10000;
-        cars.push({ name: `Car #${cars.length + 1}`, value: 10000 });
-        renderCars();
-    } else {
-        alert("Not enough money for a car!");
-    }
-}
-
-function renderCars() {
-    const div = document.getElementById("yourCars");
-    div.innerHTML = "";
-    cars.forEach(car => {
-        div.innerHTML += `${car.name} | $${car.value}<br>`;
-    });
-}
-
-setInterval(renderCars, 500);
-
+console.log("Cars system loaded (placeholder)");
